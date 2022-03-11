8053dbc4a97131a3a68db066c4be3401cc576878
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-03-11 11:48:09 +0100

    Relations between models

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Label extends Model
 {
     use HasFactory;
+
+    /**
+     * The articles that belong to the Label
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class);
+    }
 }
